4a890e3ec86ae58ce8d5f98023b3af023bd23acc
DarKS4ys <melihyardim1057@gmail.com>
2024-02-18 01:02:34 +0300

    decrease gap

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ const DynamicSystemData = dynamic(() => import('@/app/components/SystemData'), {
 
 export default function Home() {
   return (
-    <main className="min-h-screen overflow-hidden max-w-6xl mx-auto flex flex-col gap-12 items-center p-6 md:p-8">
+    <main className="min-h-screen overflow-hidden max-w-6xl mx-auto flex flex-col gap-8 items-center p-6 md:p-8">
         <h1 className="text-5xl md:text-6xl text-center font-semibold">Check My Specs</h1>
         <DynamicSystemData />
     </main>
