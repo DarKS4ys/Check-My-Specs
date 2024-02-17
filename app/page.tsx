45230c2363fc3a2ac158dba9ec169e63268edfa0
DarKS4ys <melihyardim1057@gmail.com>
2024-02-18 01:00:04 +0300

    fix window not defined

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
-import SystemDataClient from "@/app/components/SystemData";
+import dynamic from 'next/dynamic'
+ 
+const DynamicSystemData = dynamic(() => import('@/app/components/SystemData'), {
+  ssr: false,
+})
 
 export default function Home() {
   return (
     <main className="min-h-screen overflow-hidden max-w-6xl mx-auto flex flex-col gap-12 items-center p-6 md:p-8">
         <h1 className="text-5xl md:text-6xl text-center font-semibold">Check My Specs</h1>
-      <SystemDataClient />
+        <DynamicSystemData />
     </main>
   );
 }
